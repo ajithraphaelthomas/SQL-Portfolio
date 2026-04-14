954b860010c6fd04e68d5fc8b19aa4969be5bf23
Ajith Raphael Thomas <104309334+ajithraphaelthomas@users.noreply.github.com>
2026-04-14 11:01:45 -0400

    Added Day 1 SQL practice

---
+++ day1.sql
@@ -0,0 +1,12 @@
+-- Day 1 SQL Practice
+
+-- Select all data
+SELECT * FROM customers;
+
+-- Filter data
+SELECT * FROM customers
+WHERE age > 30;
+
+-- Sort data
+SELECT * FROM customers
+ORDER BY age DESC;
